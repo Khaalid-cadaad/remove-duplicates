f0ad71fedac9c33298d765daec59e65cf727b19c
unknown <khalidabdi3101@gmail.com>
2024-10-05 05:12:27 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function removeDuplicates(arr) {
+   
+    return Array.from(new Set(arr));
+}
+
+
+console.log(removeDuplicates([1, 0, 1, 0])); 
+console.log(removeDuplicates(["New", "York", "City"])); 
+console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"]));
